bf6d56fb03950ca688bb17cf3d15494dddce97b7
Linusobura <linusobura@gmail.com>
2024-07-13 11:39:14 +0000

    fixed procedure

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,25 +1,15 @@
 -- creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student
 
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+DELIMITER //
 
-CREATE TABLE IF NOT EXISTS user_avarages (
-		user_id INT PRIMARY KEY,
-			average_score DECIMAL(10, 2)
-			);
+CREATE PROCEDURE ComputeAverageScoreForUser
+    (IN user_id INT)
+BEGIN
+	    UPDATE users
+	    SET average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
+	    WHERE users.id = user_id;
+END//
 
-			DELIMITER //
-
-			CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
-
-			BEGIN
-				DECLARE avg_score DECIMAL(10, 2);
-
-				select AVG(score) INTO avg_score from corrections where corrections.user_id = user_id;
-
-				INSERT INTO user_avarages ( user_id, average_score ) VALUES(
-						 user_id, avg_score)
-						 ON DUPLICATE KEY UPDATE average_score = avg_score;
-
-					END//
-
-					DELIMITER ;
+DELIMITER ;
 
